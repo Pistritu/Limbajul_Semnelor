45465d1c1eda26c826955f2b2beeb21037a5f8b7
Laurentiu Pistirtu <larrypistritu@gmail.com>
2021-06-25 00:44:43 +0300

    updated configuration for Unity

--- Project/Unity/Assets/Configurare_v2.cs
+++ Project/Unity/Assets/Configurare_v2.cs
@@ -8,19 +8,19 @@ public class Configurare_v2 : MonoBehaviour
 {
 	// variabile pentru a salva gradul de rotatie al degetelor si al mainii
 	public Transform Mana;
-	public Transform Deget0;
 	public Transform Deget1;
 	public Transform Deget2;
 	public Transform Deget3;
 	public Transform Deget4;
+	public Transform Deget5;
 	public float R_Mana_X;
 	public float R_Mana_Y;
 	public float R_Mana_Z;
-	public float R_Deget0;
 	public float R_Deget1;
 	public float R_Deget2;
 	public float R_Deget3;
 	public float R_Deget4;
+	public float R_Deget5;
 	
 	public int Pozitie_Mana = 0;
 	
@@ -32,7 +32,7 @@ public class Configurare_v2 : MonoBehaviour
 	public bool mijlociu= false;
 	public bool inelar = false;
 	public bool mic = false;
-	public bool TEST = false;
+
 
 	
 	// preluare sunete pentru imagini
@@ -58,6 +58,7 @@ public class Configurare_v2 : MonoBehaviour
 	public AudioClip _euRoxana;
 	public AudioClip _catCosta;
 
+	// preluare imagini 
 	public GameObject img_cheama;	 
 	public GameObject img_politia;
 	public GameObject img_pompierii;
@@ -81,12 +82,19 @@ public class Configurare_v2 : MonoBehaviour
 
 	
 	//contorizare trecerea timpului
-	public float timp_difuzare = 2.0f;
-	public float timer = 0.0f;
+	public float TimpPanaLaDifuzare = 2.0f;
+	public float timerCresteLa_1s = 0.0f;
 	private int counter = 0;
 	
 	private int action_on_frame = 0;
 	
+	
+	// Faza de dezvoltare.
+    // Arata LOG-ul
+	public bool log = false;
+	
+	// Test comportament: Va fi anulat tot ce primeste de la Arduino
+	public bool TEST = false;
 	
 	void Awake () {
 		playerAudio = GetComponent <AudioSource> ();
@@ -102,16 +110,16 @@ public class Configurare_v2 : MonoBehaviour
 	void Reset()
 	{
 		
-		if(mare == false && aratator == false && mijlociu == false && inelar == false && mic == false && TEST == false){
-			timer = 0;
+		if(mare == false && aratator == false && mijlociu == false && inelar == false && mic == false){
+			timerCresteLa_1s = 0;
 			counter = 0;
 			
 			disable_all();
 
 		} // set 0
 		
-		if(mare == true && aratator == true && mijlociu == true && inelar == true && mic == true && TEST == false){
-			timer = 0;
+		if(mare == true && aratator == true && mijlociu == true && inelar == true && mic == true){
+			timerCresteLa_1s = 0;
 			counter = 0;
 			
 			disable_all();
@@ -119,9 +127,7 @@ public class Configurare_v2 : MonoBehaviour
 		} // set 0
 	}
 	
-	
-	
-	
+// dezactiveaza toate imaginile si sunetele incarcate pe display
 	void disable_all()
 	{
 		
@@ -132,7 +138,7 @@ public class Configurare_v2 : MonoBehaviour
 		img_mancare.SetActive(false);
 		img_apa.SetActive(false);	
 
-		img_1.SetActive(true);
+		img_1.SetActive(false);
 		img_2.SetActive(false);
 		img_3.SetActive(false);
 		img_4.SetActive(false);
@@ -149,7 +155,7 @@ public class Configurare_v2 : MonoBehaviour
 		img_catCosta.SetActive(false);		
 	} // end disable_all()
 	
-	// Actualizeaza pozitia Mainii si a degetelor
+// Actualizeaza pozitia Mainii si a degetelor
 	void getPosition()
 	{
 		
@@ -164,10 +170,6 @@ public class Configurare_v2 : MonoBehaviour
 		var angles_Z = Mana.transform.localRotation.eulerAngles.z;
 		int buff_Z = System.Convert.ToInt32(angles_Z);
 		R_Mana_Z = buff_Z;
-		
-		var angles0 = Deget0.transform.localRotation.eulerAngles.z;
-		int buff0 = System.Convert.ToInt32(angles0);
-		R_Deget0 = buff0;
 		
 		var angles1 = Deget1.transform.localRotation.eulerAngles.z;
 		int buff1 = System.Convert.ToInt32(angles1);
@@ -185,20 +187,24 @@ public class Configurare_v2 : MonoBehaviour
 		int buff4 = System.Convert.ToInt32(angles4);
 		R_Deget4 = buff4;
 		
+		var angles5 = Deget5.transform.localRotation.eulerAngles.z;
+		int buff5 = System.Convert.ToInt32(angles5);
+		R_Deget5 = buff5;
+		
 	} // end getPosition()
 	
 	
-	// Seteaza pozitia mainii    poz 1,2,3
+// Seteaza pozitia mainii    poz 1,2,3
 	void setPosition_mana()
 	{
 		
-		if( ((R_Mana_X <= 40) &&  (R_Mana_X > -180)) &&  ((R_Mana_Z <= 90) && (R_Mana_Z >= -90)) ){  //mana intinsa normal dar cu palma in jos
+		if( ((R_Mana_X <= 40) && (R_Mana_X > -180)) &&  ((R_Mana_Z <= 90) && (R_Mana_Z >= -90)) ){  //mana intinsa normal dar cu palma in jos
 			
 			Pozitie_Mana = 1;
 			
 		}
 		
-		if( ((R_Mana_X <= 40) &&  (R_Mana_X > -180)) &&  ((R_Mana_Z > 91) && (R_Mana_Z < 91)) ){ //mana intinsa normal dar cu palma in sus
+		if( ((R_Mana_X <= 40) && (R_Mana_X > -180)) &&  ((R_Mana_Z > 91) && (R_Mana_Z < -91)) ){ //mana intinsa normal dar cu palma in sus
 			
 			Pozitie_Mana = 2;
 			
@@ -216,35 +222,35 @@ public class Configurare_v2 : MonoBehaviour
 	
 	void setPosition_deget()
 	{
-		if((R_Deget0 <= 340) && (R_Deget0 >= 200)){
+		if((R_Deget1 <= 340) && (R_Deget1 >= 200)){
 			mare = true;
 		}
 		else{
 			mare = false;
 		}
 		
-		if((R_Deget1 <= 340) && (R_Deget1 >= 200)){
+		if((R_Deget2 <= 340) && (R_Deget2 >= 200)){
 			aratator = true;
 		}
 		else{
 			aratator = false;
 		}
 		
-		if((R_Deget2 <= 340) && (R_Deget2 >= 200)){
+		if((R_Deget3 <= 340) && (R_Deget3 >= 200)){
 			mijlociu= true;
 		}
 		else{
 			mijlociu = false;
 		}
 		
-		if((R_Deget3 <= 340) && (R_Deget3 >= 200)){
+		if((R_Deget4 <= 340) && (R_Deget4 >= 200)){
 			inelar = true;
 		}
 		else{
 			inelar = false;
 		}
 		
-		if((R_Deget4 <= 340) && (R_Deget4 >= 200)){
+		if((R_Deget5 <= 340) && (R_Deget5 >= 200)){
 			mic = true;
 		}
 		else{
@@ -260,11 +266,12 @@ public class Configurare_v2 : MonoBehaviour
 		// Variante de pozitie a degetelor
 		if(mare == false && aratator == false && mijlociu == true && inelar == true && mic == true && Pozitie_Mana == 1){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				++counter;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: Saluta");
-			
+					}
 					disable_all();
 					img_salut.SetActive(true);
 				
@@ -276,11 +283,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == false && aratator == false && mijlociu == false && inelar == true && mic == true && Pozitie_Mana == 1){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: Mancare");
-			
+					}
 					disable_all();
 					img_mancare.SetActive(true);
 				
@@ -292,11 +300,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == true && aratator == false && mijlociu == false && inelar == true && mic == true && Pozitie_Mana == 1){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: apa");
-			
+					}
 					disable_all();
 					img_apa.SetActive(true);	
 				
@@ -308,11 +317,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == false && aratator == true && mijlociu == true && inelar == true && mic == true && Pozitie_Mana == 1){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: cheama");
-			
+					}
 					disable_all();
 					img_cheama.SetActive(true);
 				
@@ -324,11 +334,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == true && aratator == false && mijlociu == true && inelar == true && mic == true && Pozitie_Mana == 1){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: politia");
-			
+					}
 					disable_all();
 					img_politia.SetActive(true);
 				
@@ -340,11 +351,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == true && aratator == true && mijlociu == true && inelar == true && mic == false && Pozitie_Mana == 1){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn:  pompierii");
-			
+					}
 					disable_all();
 					img_pompierii.SetActive(true);
 				
@@ -356,11 +368,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == true && aratator == false && mijlociu == true && inelar == true && mic == true && Pozitie_Mana == 3){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: 1");
-
+					}
 					disable_all();
 					img_1.SetActive(true);
 
@@ -372,11 +385,12 @@ public class Configurare_v2 : MonoBehaviour
 
 		if(mare == true && aratator == false && mijlociu == false && inelar == true && mic == true && Pozitie_Mana == 3){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: 2");
-			
+					}
 					disable_all();
 					img_2.SetActive(true);
 				
@@ -388,11 +402,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == true && aratator == false && mijlociu == false && inelar == false && mic == true && Pozitie_Mana == 3){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: 3");
-
+					}
 					disable_all();
 					img_3.SetActive(true);
 				
@@ -404,11 +419,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == true && aratator == false && mijlociu == false && inelar == false && mic == false && Pozitie_Mana == 3){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: 4");
-			
+					}
 					disable_all();
 					img_4.SetActive(true);
 
@@ -421,11 +437,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == false && aratator == true && mijlociu == true && inelar == true && mic == true && Pozitie_Mana == 3){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: 5");
-			
+					}
 					disable_all();
 					img_5.SetActive(true);
 				
@@ -437,11 +454,12 @@ public class Configurare_v2 : MonoBehaviour
 
 		if(mare == false && aratator == true && mijlociu == true && inelar == true && mic == false && Pozitie_Mana == 1){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: nu");
-			
+					}
 					disable_all();
 					img_nu.SetActive(true);
 				
@@ -453,11 +471,12 @@ public class Configurare_v2 : MonoBehaviour
 
 		if(mare == true && aratator == false && mijlociu == false && inelar == false && mic == true && Pozitie_Mana == 1){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: da");
-			
+					}
 					disable_all();
 					img_da.SetActive(true);
 	
@@ -469,11 +488,12 @@ public class Configurare_v2 : MonoBehaviour
 
 		if(mare == false && aratator == false && mijlociu == true && inelar == true && mic == false && Pozitie_Mana == 3){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: Va multumesc");
-				
+					}
 					disable_all();
 					img_vaMultumesc.SetActive(true);
 
@@ -485,11 +505,12 @@ public class Configurare_v2 : MonoBehaviour
 
 		if(mare == false && aratator == false && mijlociu == true && inelar == true && mic == true && Pozitie_Mana == 2){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: nevoie medic");
-			
+					}
 					disable_all();
 					img_nevoieMedic.SetActive(true);
 
@@ -502,11 +523,12 @@ public class Configurare_v2 : MonoBehaviour
 
 		if(mare == false && aratator == false && mijlociu == false && inelar == true && mic == true && Pozitie_Mana == 2){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: nevoie ajutor");
-	
+					}
 					disable_all();
 					img_nevoieAjutor.SetActive(true);
 
@@ -518,11 +540,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == true && aratator == false && mijlociu == false && inelar == false && mic == false && Pozitie_Mana == 2){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: ma simt bine");
-			
+					}
 					disable_all();
 					img_maSimtBine.SetActive(true);
 
@@ -534,11 +557,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == false && aratator == true && mijlociu == true && inelar == true && mic == true && Pozitie_Mana == 2){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: eu Roxana");
-			
+					}
 					disable_all();
 					img_euRoxana.SetActive(true);
 
@@ -550,11 +574,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == false && aratator == true && mijlociu == true && inelar == true && mic == false && Pozitie_Mana == 2){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn: vino cu mine");
-					
+					}
 					disable_all();
 					img_vinoCuMine.SetActive(true);
 		
@@ -566,11 +591,12 @@ public class Configurare_v2 : MonoBehaviour
 		
 		if(mare == true && aratator == true && mijlociu == true && inelar == true && mic == false && Pozitie_Mana == 2){
 
-			if(timer > timp_difuzare){
+			if(timerCresteLa_1s > TimpPanaLaDifuzare){
 				counter++;
 				if(counter == 1){
+					if(log == true){
 					Debug.Log ("Semn:  cat costa?");
-			
+					}
 					disable_all();
 					img_catCosta.SetActive(true);
 				
@@ -580,22 +606,24 @@ public class Configurare_v2 : MonoBehaviour
 			}
 		} //end cat costa? (palma in sus)
 
-	Debug.Log (TEST);	
-		if(TEST == true){
-
-			if(timer > timp_difuzare){
-				counter++;
-				if(counter == 1){
-					Debug.Log ("Semn:  Test img si sunet");
-			
-					disable_all();
-					img_1.SetActive(true);
-
-					playerAudio.clip = _1;
-					playerAudio.Play ();			
-				}
-			}
-		} //TEST
+	    // Debug.Log (TEST);	
+//		if(TEST == true){
+//
+//			if(timerCresteLa_1s > TimpPanaLaDifuzare){
+//				counter++;
+//				if(counter == 1){
+//					if(log == true){
+//					Debug.Log ("Semn:  Test img si sunet");
+//					}
+//					disable_all();
+//					img_1.SetActive(true);
+//
+//					playerAudio.clip = _1;
+//					playerAudio.Play ();			
+//				}
+//			}
+//		} //TEST
+		
 	} //end start_action()
 
 
@@ -604,17 +632,30 @@ public class Configurare_v2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		timer += Time.deltaTime;
+		timerCresteLa_1s += Time.deltaTime;
 		action_on_frame++;
 		
-		if (action_on_frame == 3 ) //dupa 3 frame-uri sa se actualizeze datele)
-		{			
-			getPosition();
-			setPosition_mana();
-			setPosition_deget();
-			Reset();
-			start_action();
-			action_on_frame = 0;
+		if (TEST == false){
+			
+			if (action_on_frame == 3 ) //dupa 3 frame-uri sa se actualizeze datele)
+			{			
+				getPosition();
+				setPosition_mana();
+				setPosition_deget();
+				Reset();
+				start_action();
+				action_on_frame = 0;
+			}
+		}
+		if (TEST == true){
+			
+			if (action_on_frame == 3 ) //dupa 3 frame-uri sa se actualizeze datele)
+			{			
+				setPosition_mana();
+				Reset();
+				start_action();
+				action_on_frame = 0;
+			}
 		}
     }  //end update
 
